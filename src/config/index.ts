d6334f8e13cc21c893eb04d7a6129aa3230693c1
Huy Doan <huy11900@gmail.com>
2022-11-20 10:15:53 +0700

    QG-14 - Update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ export default {
     port: process.env.PORT ?? '5000',
   },
   jwt: {
-    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
-    accessTokenExpired: process.env.ACCESS_TOKEN_EXPIRED,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET ?? '',
+    accessTokenExpired: process.env.ACCESS_TOKEN_EXPIRED ?? '6h',
   },
 };
