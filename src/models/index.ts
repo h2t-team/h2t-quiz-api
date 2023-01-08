60875f3867ee9efe6088da59b9aa9c2d44b2fa29
Huy Doan <huy11900@gmail.com>
2023-01-08 13:03:19 +0700

    Update sequelize connection

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,14 +2,14 @@ import { Sequelize } from 'sequelize';
 import config from '../config';
 import { initModels } from './init-models';
 
-// export const sequelize = new Sequelize(config.server.dtbUri, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-export const sequelize = new Sequelize(config.server.dtbUri);
+export const sequelize = new Sequelize(config.server.dtbUri, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+// export const sequelize = new Sequelize(config.server.dtbUri);
 
 export const models = initModels(sequelize);
